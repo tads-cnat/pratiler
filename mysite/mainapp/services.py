6bbc449e3697896ad0e544760baacb09a8dc12c4
deborasamara <deborasamara.ro@gmail.com>
2024-08-13 21:53:58 -0300

    Co-authored-by: Ester Oliveira <oliver.ester.2004@gmail.com>

---
+++ mysite/mainapp/services.py
@@ -0,0 +1,32 @@
+from .models import Comentario
+from datetime import datetime
+from datetime import timedelta
+
+class VerLivrosPopularesService():
+    def VerLivrosPopulares():
+        #ver comentários mais recentes
+        data_recente = datetime.today() - timedelta(days=15) # cálculo de 15 dias atras
+        comentarios_recentes = []
+        comentarios = Comentario.objects.all() # todos os comentários
+        for i in comentarios:
+            if i.data_hora() <= data_recente:
+                comentarios_recentes.append(i) # adiciona os comentários mais recentes
+        #ver quais são os livros dos comentários recentes
+        livros = [] # vetor de livros
+        if(len(comentarios_recentes) >= 3):
+            for i in comentarios_recentes:
+                if not(i.livro in livros): # evitar repetição
+                    livros.append(i.livro) # adiciona livro do comentário sem repetição
+        #ver quais são os livros com mais comentários recentes
+        livros_populares = [] # livros populares
+        if len(livros) >= 3:
+            for x in range(0, 3): # só vai mostrar três livros
+                mais_comentarios = 0
+                livro_popular = livros[0] 
+                for l in livros: 
+                    if l.comentario_set.count() > mais_comentarios:
+                        mais_comentarios = l.comentario_set.count()
+                        livro_popular = l
+                livros_populares.append(livro_popular)
+                livros.remove(livro_popular)
+        return livros_populares # retorna os 3 livros mais populares 
--- mysite/mainapp/views.py
+++ mysite/mainapp/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Comentario, Usuario, Curtida
+from .services import VerLivrosPopularesService
 
 # Create your views here.
 class VerFeedView(View):
     def get(self, request, *args, **kwargs):
         comentarios = Comentario.objects.all()
         comentarios_relevantes = []
-        if comentarios == []:
-            mensagem = "Não há comentários relevantes no momento."
-            return render(request, 'mainapp/feed_relevantes.html', {'mensagem':mensagem})
+        # if comentarios == []:
+        #     mensagem = "Não há comentários relevantes no momento."
+        #     return render(request, 'mainapp/feed_relevantes.html', {'mensagem':mensagem})
+        # else:
+        if len(comentarios) > 10:
+            for i in range(0, 10):
+                if comentarios[i].curtida_set.count() > 10: 
+                    comentarios_relevantes.append(comentarios[i])
         else:
-            if len(comentarios) > 10:
-                for i in range(0, 10):
-                    if comentarios[i].curtida_set.count() > 10: 
-                        comentarios_relevantes.append(comentarios[i])
-            else:
-                for i in comentarios:
-                    if i.curtida_set.count() > 10:
-                        comentarios_relevantes.append(i)
-        return render(request, 'mainapp/feed_relevantes.html', {'comentarios_relevantes':comentarios_relevantes})
-    
-    #def post(self, request, *args, **kwargs):
+            for i in comentarios:
+                if i.curtida_set.count() > 10:
+                    comentarios_relevantes.append(i)
+        return render(request, 'templates/feed_relevantes.html', {'comentarios_relevantes':comentarios_relevantes})
+
+class VerLivrosPopulares(View):
+    def get(self, request, *args, **kwargs):
+        servico = VerLivrosPopularesService()
+        livros_populares = servico.VerLivrosPopulares()
+        if livros_populares == []: # se não tiver nada, mostrar mensagem de que não tem nenhum livro popular no momento
+            mensagem = "Não há livros populares no momento."
+            return render(request, 'templates/livros_populares.html', {'mensagem': mensagem})
+        # renderização dos livros populares 
+        return render(request, 'templates/livros_populares.html', {'livros_populares': livros_populares})
+
+
+        
+        
+
 
