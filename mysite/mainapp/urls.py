4383225524d869742bef89129de49ec11a7959e9
Lipeeeeeee <favasconcelos09@gmail.com>
2024-09-01 11:28:02 -0300

    Criação do Gerenciar Estante

--- mysite/mainapp/urls.py
+++ mysite/mainapp/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     #path('<slug:username>/seguir/', views.SeguirLeitorView.as_view(), name="seguir_leitor"),
     path('feed/', views.VerFeedView.as_view(), name="feed"),
     path('livros_populares/', views.VerLivrosPopulares.as_view(), name="livros_populares"),
+    path('feed_seguindo/', views.VerFeedSeguindoView.as_view(), name="seguindo"),
+    path('minha_estante/', views.VerMinhaEstante.as_view(), name="minha_estante"),
     path('livros/', views.GerenciarLivrosView.as_view(), name="index"),
     path('livros/add/', views.GerenciarLivrosView.get_adicionar, name="adicionar"),
     path('livros/adicionar/', views.GerenciarLivrosView.as_view(), name="adicionar_livros"),
@@ -19,6 +21,5 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"), # Chaves
     path('leitor/<str:username>', views.paginaLeitor, name='pagina_leitor'), # Chaves
     path('livros_pesquisa/', views.livros_pesquisa, name="livros_pesquisa"), #Chaves
-    path('feed_seguindo/', views.VerFeedSeguindoView.as_view(), name="seguindo"),
     path('<str:username>/seguir/', views.SeguirLeitorView.as_view(), name="seguir_leitor")
 ]
