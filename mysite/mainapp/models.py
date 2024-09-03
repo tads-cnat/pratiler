e5ec77a33929b18c0b31912a4c1db6e4cc64e5c2
Lipeeeeeee <favasconcelos09@gmail.com>
2024-09-03 17:47:07 -0300

    Arrumei uma redundância

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -37,7 +37,7 @@ class Livro(models.Model):
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.titulo + " " + self.autor.nome
+        return self.titulo + " - " + self.autor.nome
     
 class Comentario(models.Model):
     livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
