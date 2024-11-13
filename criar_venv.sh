5587511b8e3ceb2b85cb10c87f35dd924efe5db0
João Roberto <devjoaoroberto@gmail.com>
2024-11-12 22:38:26 -0300

    Adicionando o script de automação linux e windows

---
+++ criar_venv.sh
@@ -0,0 +1,18 @@
+# INSTALAÇÃO DO VENV, ATIVAÇÃO E INSTALAÇÃO DO REQUIREMENTS (LINUX)
+
+#!/bin/bash
+
+# Nome do ambiente virtual
+ENV_NAME="venv"
+
+# Cria o ambiente virtual
+python3 -m venv $ENV_NAME
+
+# Ativa o ambiente virtual
+source $ENV_NAME/bin/activate
+
+echo "Ambiente virtual '$ENV_NAME' criado e ativado!"
+
+pip install -r requirements.txt
+
+echo "Instalando o requirements.txt com sucesso!"
