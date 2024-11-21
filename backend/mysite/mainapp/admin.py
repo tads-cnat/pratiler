e2d71e3f8ff1a5d1a2d830c32b6ae70c31205c1f
Walber Ranniere <walber.ranniere147@gmail.com>
2024-11-21 10:27:12 -0300

    chore: Integracion on the main

--- backend/mysite/mainapp/admin.py
+++ backend/mysite/mainapp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Leitor
+from .models import Leitor,Autor, Livro
 
 admin.site.register(Leitor)
+
+admin.site.register(Autor)
+admin.site.register(Livro)
+
--- backend/mysite/mainapp/schemas.py
+++ backend/mysite/mainapp/schemas.py
@@ -1,4 +1,6 @@
-from ninja import Schema
+from ninja import ModelSchema, Schema
+from django.contrib.auth.models import User
+from .models import Autor, Livro, Leitor
 
 class SignInSchema(Schema):
     email: str
@@ -8,3 +10,33 @@ class RegisterSchema(Schema):
     email: str
     password: str
     username: str
+
+class AutorSchema(ModelSchema):
+    class Config:
+        model = Autor
+        model_fields = '__all__'  # Defini todos os campos para a API
+
+class LivroSchema(ModelSchema):
+    class Config:
+        model = Livro 
+        model_fields = '__all__'
+
+class UserSchema(ModelSchema):
+    class Config:
+        model = User
+        model_fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class LeitorSchema(ModelSchema):
+    class Config:
+        model = Leitor
+        model_fields = '__all__'
+
+# class ResenhaSchema(ModelSchema):
+#     class Config:
+#         model = Resenha 
+#         model_fields = ['id', 'livro', 'leitor', 'texto', 'data_hora']
+
+# class ComentarioSchema(ModelSchema):
+#     class Config:
+#         model = Comentario  
+#         model_fields = ['id', 'livro', 'leitor', 'texto', 'data_hora', 'pagina_final']
