e9fc42dc5fdc7a07f8928e90203c0dd7f31e06de
FelipeAlves14 <favasconcelos09@gmail.com>
2024-11-29 17:27:45 -0300

    fix: solucionando bugs no api.py

--- backend/mysite/mainapp/api.py
+++ backend/mysite/mainapp/api.py
@@ -56,7 +56,7 @@ def register(request, payload: schemas.RegisterSchema):
 @api.get("/autores", response=list[AutorSchema])
 def listar_autores(request):
     """Lista todos os autores."""
-    return Autor.objects.all()
+    return models.Autor.objects.all()
 
 @api.get("/livros", response=list[LivroSchema])
 def listar_livros(request):
@@ -82,23 +82,23 @@ def listar_livros(request):
 @api.get("/leitores", response=list[LeitorSchema])
 def listar_leitores(request):
     """Lista todos os leitores."""
-    return Leitor.objects.all()
+    return models.Leitor.objects.all()
 
 @api.get("/leitores/{leitor_id}", response=LeitorSchema)
 def listar_leitor(request, leitor_id: int):
     """Lista um leitor específico."""
     try:
-        leitor = Leitor.objects.get(id=leitor_id)
+        leitor = models.Leitor.objects.get(id=leitor_id)
         return leitor
-    except Leitor.DoesNotExist:
+    except models.Leitor.DoesNotExist:
         return JsonResponse({"detalhe": "leitor não encontrado"}, status=404)
 
 @api.get("/leitores/{leitor_id}/user", response=UserSchema)
 def listar_user_do_leitor(request, leitor_id: int):
     """Lista um User (Django) específico"""
     try:
-        leitor = Leitor.objects.get(id=leitor_id)
+        leitor = models.Leitor.objects.get(id=leitor_id)
         user = leitor.user_django  # Pega o User relacionado ao Leitor
         return user
-    except Leitor.DoesNotExist:
+    except models.Leitor.DoesNotExist:
         return JsonResponse({"detalhe": "leitor não encontrado"}, status=404)
