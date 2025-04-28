22349d87ce04c0d68c1a591fe8c8c63dd4203aac
FelipeAlves14 <favasconcelos09@gmail.com>
2025-04-28 17:18:01 -0300

    feat: implementa tab de resenhas e escrever resenhas

--- frontend/pratiler-frontend/src/components/Perfil/Tabs/Resenhas/index.jsx
+++ frontend/pratiler-frontend/src/components/Perfil/Tabs/Resenhas/index.jsx
@@ -4,14 +4,18 @@ import { internalAxios } from "../../../Global/axiosInstances";
 
 /* Components */
 import { SemResultados } from "../../../SemResultado";
+import EscreverResenha from "./escreverResenha";
 
 /* CSS */
-import resenhaCss from "../../../../assets/css/Perfil/Resenhas.module.css";
+import resenhaCss from "../../../../assets/css/Perfil/Resenhas/Resenhas.module.css";
+import { useAuthStore } from "../../../Global/authStore";
 
 export function Resenhas(props) {
   const { username } = props;
+  const { user } = useAuthStore();
   const [resenhas, setResenhas] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isWriting, setWriting] = useState(false);
 
   async function loadResenhas() {
     await internalAxios
@@ -33,40 +37,49 @@ export function Resenhas(props) {
       {loading ? (
         <p>Carregando...</p>
       ) : (
-        <div className={resenhaCss.resenhas}>
-          {resenhas.length > 0 ? (
-            resenhas.map((resenha, index) => (
-              <div className={resenhaCss.cardResenhas} key={index}>
-                <div className={resenhaCss.cardResenhasContent}>
-                  <p>
-                    <strong>
-                      {resenha.livro.titulo} - {resenha.livro.autor.nome}
-                    </strong>
-                  </p>
-                  <p>{resenha.titulo}</p>
-                  <button
-                    className={resenhaCss.botaoLerResenha}
-                  >
-                    Ler resenha
-                  </button>
-                </div>
-                <img src="{{ resenha.livro.capa }}" />
-              </div>
-            ))
+        <>
+          {isWriting ? (
+            <EscreverResenha setWriting={setWriting} />
           ) : (
-            <>
-              <SemResultados
-                titulo="Não há resenhas postadas ainda"
-                tamanho="P"
-              />
-            </>
+            <div className={resenhaCss.resenhas}>
+              {resenhas.length > 0 ? (
+                resenhas.map((resenha, index) => (
+                  <div className={resenhaCss.cardResenhas} key={index}>
+                    <div className={resenhaCss.cardResenhasContent}>
+                      <p>
+                        <strong>
+                          {resenha.livro.titulo} - {resenha.livro.autor.nome}
+                        </strong>
+                      </p>
+                      <p>{resenha.titulo}</p>
+                      <button className={resenhaCss.botaoLerResenha}>
+                        Ler resenha
+                      </button>
+                    </div>
+                    <img src={resenha.livro.capa} />
+                  </div>
+                ))
+              ) : (
+                <>
+                  <SemResultados
+                    titulo="Não há resenhas postadas ainda"
+                    tamanho="P"
+                  />
+                </>
+              )}
+              {username === user.username && (
+                <button
+                  onClick={() => {
+                    setWriting(true);
+                  }}
+                  className={resenhaCss.botaoEscreverResenha}
+                >
+                  Escrever Resenha
+                </button>
+              )}
+            </div>
           )}
-          <button
-            className={resenhaCss.botaoEscreverResenha}
-          >
-            Escrever Resenha
-          </button>
-        </div>
+        </>
       )}
     </>
   );
