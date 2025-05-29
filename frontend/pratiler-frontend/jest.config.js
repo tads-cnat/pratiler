554a2a5c464fd5d28c3cb05208cc4309728ddb89
deborasamara <deborasamara.ro@gmail>
2025-05-29 15:45:04 -0300

    ci: adiciona configuração inicial do Jest para testes unitários

---
+++ frontend/pratiler-frontend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+};
+
+// Código arquitetado a partir de:
+// Continuação futura - Teste no front end
+// https://www.youtube.com/watch?v=HLgY_Cmqe14&list=LL&index=1
+// https://jestjs.io/pt-BR/docs/getting-started
