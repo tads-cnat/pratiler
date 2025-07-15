701f3b8aa5c1e8348dbb7177b0645e9b313b6c84
FelipeAlves14 <favasconcelos09@gmail.com>
2025-07-15 10:49:56 -0300

    feat: cria arquivo de configuração Cypress

--- frontend/pratiler-frontend/cypress.config.js
+++ frontend/pratiler-frontend/cypress.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost",
+    viewportWidth: 1280,
+		viewportHeight: 936,
+		testIsolation: false,
+		retries: { openMode: 2 },
   },
+  env: {
+    API_BASE_URL: "http://localhost:8000/api"
+  }
 });
