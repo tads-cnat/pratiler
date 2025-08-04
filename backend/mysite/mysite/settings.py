4752848c53b7e16f2d450bed931fee08cd6ebf68
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-08-04 17:38:53 -0300

    :sparkles: adicionando arquivos manifestos backend

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -84,21 +84,28 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'pratiler'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'), 
+DEV = os.getenv('DEV', '1')
+DATABASES = {}
+print("DEV", DEV)
+if os.getenv('DEV') == "0":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'pratiler'),
+            'USER': os.getenv('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+            'HOST': os.getenv('POSTGRES_HOST', 'db'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'), 
+        }
     }
-} if os.getenv('DEV') == 0 else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+print("DATABASES", DATABASES)
 
 
 # Password validation
