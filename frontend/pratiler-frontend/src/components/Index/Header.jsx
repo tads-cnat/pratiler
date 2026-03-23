7ef2dad51954b7c2d23790990baa9a52c9e8c10b
deborasamara <deborasamara.ro@gmail.com>
2026-03-23 18:24:59 -0300

    feat: adicionar renderização do link da logo dependendo da prop de autenticação

--- frontend/pratiler-frontend/src/components/Index/Header.jsx
+++ frontend/pratiler-frontend/src/components/Index/Header.jsx
@@ -8,12 +8,18 @@ import headerCss from '../../assets/css/Index/Header.module.css';
 import pratilerLogo from '../../assets/img/pratiler-logo.png';
 import iconIn from '../../assets/img/icon-going.png';
 
-export function Header() {
+export function Header({isAuthenticated}) {
   return (
     <header className={headerCss.header}>
-      <Link to="/">
+      {isAuthenticated ? (
+      <Link to="/livros">
         <img className={headerCss.logo} src={pratilerLogo} alt="Logo do Projeto Pratiler" />
       </Link>
+      ): (
+        <Link to="/">
+        <img className={headerCss.logo} src={pratilerLogo} alt="Logo do Projeto Pratiler" />
+      </Link>
+      )}
       <div className={headerCss.buttons}>
         <Link to="/login" className={headerCss.box_in}>
           <button className={headerCss.box_in}>
