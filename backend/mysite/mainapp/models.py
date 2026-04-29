a3fa1952a6899a2b1cf1449d32c9e186cffa9dc4
deborasamara <deborasamara.ro@gmail.com>
2026-04-29 11:22:32 -0300

    fix: adicionar importação antiga de tradução no modelo Leitor

--- backend/mysite/mainapp/models.py
+++ backend/mysite/mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 class Leitor(AbstractUser):
     email = models.EmailField(unique=True) # e-mail unico
